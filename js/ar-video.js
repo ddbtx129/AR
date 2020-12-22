e0f26c8fba155e1b545776b0cba72dfcfeffc055
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-22 11:26:11 +0900

    Update ar-video.js

--- js/ar-video.js
+++ js/ar-video.js
@@ -4,8 +4,9 @@ window.alert('0');
     param = GetQueryString();
 window.alert('1');
     var video = param["o"] != '' ? "article/" + param["o"] + ".mp4" : 'article/notfound_video.mp4';
-    window.alert(video);
+    
     if (IsFile(video)) {
+        window.alert(video);
         document.getElementById("ar-video").setAttribute("src", video);
     } else {
         Err_Exit('対象動画が見つかりません。');
@@ -14,6 +15,7 @@ window.alert('1');
     var marker = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
 
     if (IsFile(marker)) {
+        window.alert(marker);
         document.getElementById("ar-marker").setAttribute("url", marker);
     } else {
         Err_Exit('対象追跡データが見つかりません。');
