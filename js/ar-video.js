e370ea13b60dc8a16557f46c5b84888a59738840
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-22 11:45:53 +0900

    Update ar-video.js

--- js/ar-video.js
+++ js/ar-video.js
@@ -1,23 +1,22 @@
-﻿
+
 function getObject() {
-window.alert('0');
+
     param = GetQueryString();
-window.alert(param["o"]);
-    var video = param["o"] != '' ? "article/" + param["o"] + ".mp4" : 'article/notfound_video.mp4';
+
+    var video = document.getElementById("ar-video");
+    var vObj = param["o"] != '' ? "article/" + param["o"] + ".mp4" : 'article/notfound_video.mp4';
     
-    if (IsFile(video)) {
-        window.alert(video);
-        document.getElementById("ar-video").setAttribute("src", video);
-        window.alert(video);
+    if (IsFile(vObj)) {
+        video.setAttribute("src", vObj);
     } else {
         Err_Exit('対象動画が見つかりません。');
     }
-    window.alert('marker0');
-    var marker = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
-    window.alert('marker1');
-    if (IsFile(marker)) {
-        window.alert(marker);
-        document.getElementById("ar-marker").setAttribute("url", marker);
+
+    var marker = document.getElementById("ar-video");
+    var mObj = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
+
+    if (IsFile(mObj)) {
+        marker.getElementById("ar-marker").setAttribute("url", mObj);
     } else {
         Err_Exit('対象追跡データが見つかりません。');
     }
