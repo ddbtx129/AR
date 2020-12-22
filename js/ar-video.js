25e43279f5c2bec712e2c78596ec1ed51dcea183
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-22 15:03:08 +0900

    Add files via upload

--- js/ar-video.js
+++ js/ar-video.js
@@ -7,7 +7,7 @@ function getObject() {
     var vObj = param["o"] != '' ? "article/" + param["o"] + ".mp4" : 'article/notfound_video.mp4';
 
     video.setAttribute("src", vObj);
-
+    window.alert(vObj);
     //if (IsFile(vObj)) {
     //    widnow.alert("ファイル有り");
     //    video.setAttribute("src", vObj);
@@ -19,6 +19,7 @@ function getObject() {
     var mObj = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
 
     marker.setAttribute("url", mObj);
+    window.alert(mObj);
 
     //if (IsFile(mObj)) {
     //    marker.getElementById("ar-marker").setAttribute("url", mObj);
