c9b6312e93023c344932902f685b176ca6a218e0
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-22 10:38:02 +0900

    Add files via upload

---
+++ js/ar-video.js
@@ -0,0 +1,40 @@
+﻿
+function getVideo() {
+    param = GetQueryString();
+
+
+    var video = article / notfound_video.mp4;
+
+    if (param["o"] != '' && IsFile("article/" + param["o"] + ".mp4")) {
+        //document.getElementById("ar-video").setAttribute("src", "article/" + param["o"] + ".mp4");
+        video = "article/" + param["o"] + ".mp4";
+    } else {
+        Err_Exit('対象動画が見つかりません。');
+    }
+
+    return video;
+
+    //var marker = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
+
+    //if (IsFile(marker)) {
+    //    document.getElementById("ar-marker").setAttribute("url", marker);
+    //} else {
+    //    Err_Exit('対象追跡データが見つかりません。');
+    //}
+
+};
+
+function getMarker() {
+
+    param = GetQueryString();
+
+    var marker = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
+
+    if (IsFile(marker)) {
+        //document.getElementById("ar-marker").setAttribute("url", marker);
+    } else {
+        Err_Exit('対象追跡データが見つかりません。');
+    }
+
+    return marker;
+};
