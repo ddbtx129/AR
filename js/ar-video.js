e5d217dda4e73f8bb11573f02b672ecc7c821a13
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-22 14:32:31 +0900

    Update ar-video.js

--- js/ar-video.js
+++ js/ar-video.js
@@ -5,24 +5,19 @@ function getObject() {
 
     var video = document.getElementById("ar-video");
     var vObj = param["o"] != '' ? "article/" + param["o"] + ".mp4" : 'article/notfound_video.mp4';
-    video.setAttribute("src", vObj);
-    test1(vObj);
-    IsFile(vObj, function (res) {
-        if (res) {
-            video.setAttribute("src", vObj);
-        } else {
-            Err_Exit('対象動画が見つかりません。');
-        }
-    });
+
+    if (IsFile(vObj)) {
+        video.setAttribute("src", vObj);
+    } else {
+        Err_Exit('対象動画が見つかりません。');
+    }
 
     var marker = document.getElementById("ar-video");
     var mObj = (param["m"] != '') ? "pattern/pattern-" + param["m"] + ".patt" : "pattern/pattern-0.patt";
 
-    IsFile(mObj, function (res) {
-        if (res) {
-            marker.getElementById("ar-marker").setAttribute("url", mObj);
-        } else {
-            Err_Exit('対象追跡データが見つかりません。');
-        }
-    });
+    if (IsFile(mObj)) {
+        marker.getElementById("ar-marker").setAttribute("url", mObj);
+    } else {
+        Err_Exit('対象追跡データが見つかりません。');
+    }
 };
