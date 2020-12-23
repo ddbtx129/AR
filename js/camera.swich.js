0f5e4a85f6851ab0e8b5ff202c3e680db4e4677c
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-23 09:34:10 +0900

    Add files via upload

--- js/camera.swich.js
+++ js/camera.swich.js
@@ -42,3 +42,62 @@ function stopStreamedVideo(videoElem) {
 
     videoElem.srcObject = null;
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var image = document.querySelector('#scrshot');
+
+    //スナップショットボタン
+    image.addEventListener("click", function (e) {
+
+        var res = window.confirm("スナップショットを行います。");
+
+        if (res == true) {
+
+            e.preventDefault();
+
+            var video = document.querySelector('video');
+            var snap = takeSnapshot(video);
+
+            location.reload();
+
+            var win = window.open();
+            win.document.write("<img src='" + snap + "'/>");
+        }
+    })
+});
+
+//スナップショットを撮る
+function takeSnapshot(video) {
+
+    var resizedCanvas = document.createElement("canvas");
+    var resizedContext = resizedCanvas.getContext("2d");
+    var width = video.videoWidth;
+    var height = video.videoHeight;
+    var aScene = document.querySelector("a-scene").components.screenshot.getCanvas("perspective");
+
+    if (width && height) {
+
+        //videoのサイズをキャンバスにセット
+        resizedCanvas.width = width;
+        resizedCanvas.height = height;
+
+        //キャンバスにvideoをコピー
+        resizedContext.drawImage(video, 0, 0, width, height);
+
+        //カメラの画角でar側の縮小処理を変える
+        if (width > height) {
+            // 横長（PC)
+            resizedContext.drawImage(aScene, 0, 0, width, height);
+        } else {
+            // 縦長（スマホ）
+            var scale = height / width;
+            var scaledWidth = height * scale;
+            var marginLeft = (width - scaledWidth) / 2;
+
+            resizedContext.drawImage(aScene, marginLeft, 0, scaledWidth, height);
+        }
+
+        return resizedCanvas.toDataURL('image/png');
+    }
+}
