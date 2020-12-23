cfc98c35446668a1afce567622c4eb2db2f230be
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-23 23:28:32 +0900

    Add files via upload

--- js/ar-video.js
+++ js/ar-video.js
@@ -23,33 +23,36 @@ function getNftVideo() {
     var audio = document.getElementById("ar-audio");
     
     var vObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
-    window.alert(String(vObj));
+
     video.setAttribute("src", vObj);
     audio.setAttribute("src", vObj);
 };
 
 function getNftObject() {
-    window.alert("21");
+
     var arg = GetQueryString();
 
     var nft = document.getElementById("ar-gltf");
     var nObj = !(arg["o"]) ? '' : path + 'article/nftobject/' + arg["o"] + ".gltf";
-    window.alert(nObj);
-    window.alert("22");
+
     nft.setAttribute("gltf-model", nObj);
-    nft.setAttribute("scale", "10 10 10");
+    nft.setAttribute("scale", "20 20 20");
     nft.setAttribute("position", "0 0 0");
-    //nft.setAttribute("rotation", "-45 0 0");
-    window.alert("23")
+    nft.setAttribute("rotation", "-45 0 0");
 };
 
 function getNftImage() {
-    window.alert("11");
+    
     var arg = GetQueryString();
-    var nft = document.getElementById("ar-nft");
 
-    var nObj = !(arg["m"]) ? '' : path + 'ImageDescriptors/' + arg["m"] + '/' + arg["m"];
-    window.alert(nObj);
-    window.alert("12");
+    var nft = document.getElementById("ar-nft");
+    var nObj;
+
+    if ((arg["m1"]) && (arg["m2"])) {
+        nObj = path + 'ImageDescriptors/' + arg["m1"] + '/' + arg["m2"];
+    } else {
+        nObj = !(arg["m"]) ? '' : path + 'ImageDescriptors/' + arg["m"] + '/' + arg["m"];
+    }
+
     nft.setAttribute("url", nObj);
 };
