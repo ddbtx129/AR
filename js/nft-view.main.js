88cc6dfa0bcf81144b45653d12e02a3c7588d78f
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-24 23:57:06 +0900

    Add files via upload

--- js/nft-view.main.js
+++ js/nft-view.main.js
@@ -3,6 +3,7 @@ var nft = document.getElementById("ar-gltf");
 window.addEventListener('DOMContentLoaded', function () {
 
     // ↓ rotation 切替
+
     var anglebtn = document.querySelector('#swAngle');
     var parallelbtn = document.querySelector('#swParallel');
 
@@ -22,7 +23,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     parallelbtn.addEventListener('click', function () {
         if (!parallelbtn.classList.contains('current')) {
-            arRotation = '-135 0 0';
+            arRotation = '-90 0 0';
             nft.setAttribute('rotation', AFRAME.utils.coordinates.stringify(arRotation));
 
             parallelbtn.classList.add('current');
@@ -32,3 +33,41 @@ window.addEventListener('DOMContentLoaded', function () {
     // ↑
 });
 
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var wrapPos = nft.getAttribute('position');
+    //wrapPos.x += 0;
+    //wrapPos.y -= 0.5;
+    //wrapPos.z -= 8;
+    //self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(wrapPos));
+    //self.wrap.setAttribute('rotation', '0 0 0');
+
+    var prevPageY;
+    var prevPageX;
+    var zoomRate = 1;
+
+    webArViewer.scene.addEventListener('touchstart', function (e) {
+        var event = e.changedTouches ? e.changedTouches[0] : e;
+        prevPageY = event.pageY;    // 縦軸
+        prevPageX = event.pageX;    // 横軸
+    })
+
+    webArViewer.scene.addEventListener('touchmove', function (e) {
+        var event = e.changedTouches ? e.changedTouches[0] : e;
+
+        if (prevPageY) {
+            if ((zoomRate + (prevPageY - event.pageY) / webArViewer.scene.clientHeight / 5) > 0.1) {
+                zoomRate += (prevPageY - event.pageY) / webArViewer.scene.clientHeight / 5;
+                AFRAME.utils.entity.setComponentProperty(wrap, 'animation__scale', {
+                    property: 'scale', dur: 5, easing: 'linear', loop: false, to: zoomRate + ' ' + zoomRate + ' ' + zoomRate
+                });
+            }
+        }
+    });
+
+    webArViewer.scene.addEventListener('touchend', function (e) {
+        prevPageY = null;
+    })
+
+});
