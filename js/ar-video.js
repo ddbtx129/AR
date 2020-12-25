3b2629ff44f95ce7a34450eb0f64f970839d85da
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-25 10:59:43 +0900

    Add files via upload

--- js/ar-video.js
+++ js/ar-video.js
@@ -5,12 +5,26 @@ function getArVideo() {
     var arg = GetQueryString();
 
     var video = document.getElementById("ar-video");
-    var vObj = !(arg["o"]) ? 'article/video/notfound_video.mp4' : 'article/video/' + arg["o"] + '.mp4';
+    //var vObj = !(arg["o"]) ? 'article/video/notfound_video.mp4' : 'article/video/' + arg["o"] + '.mp4';
+    var vObj;
+
+    if ((arg["o1"]) && (arg["o2"])) {
+        sObj = 'article/video/' + arg["o1"] + '/' + arg["o2"] + '.mp4';
+    } else {
+        sObj = !(arg["o"]) ? 'article/video/notfound_video.mp4' : 'article/video/' + arg["o"] + '.mp4';
+    }
 
     video.setAttribute("src", vObj);
     
     var marker = document.getElementById("ar-marker");
-    var mObj = !(arg["m"]) ? 'pattern/pattern-def.patt' : 'pattern/pattern-' + arg["m"] + '.patt';
+    //var mObj = !(arg["m"]) ? 'pattern/pattern-def.patt' : 'pattern/pattern-' + arg["m"] + '.patt';
+    var mObj;
+
+    if ((arg["m1"]) && (arg["m2"])) {
+        nObj = path + 'pattern/' + arg["m1"] + '/pattern-' + arg["m2"] + '.patt';
+    } else {
+        nObj = !(arg["m"]) ? 'pattern/pattern-def.patt' : 'pattern/pattern-' + arg["m"] + '.patt';
+    }
 
     marker.setAttribute("url", mObj);
 };
@@ -22,7 +36,14 @@ function getNftVideo() {
     var video = document.getElementById("ar-video");
     var audio = document.getElementById("ar-audio");
     
-    var vObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
+    //var vObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
+    var vObj;
+
+    if ((arg["o1"]) && (arg["o2"])) {
+        nObj = path + 'article/video/' + arg["o1"] + '/' + arg["o2"] + '.mp4';
+    } else {
+        nObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
+    }
 
     video.setAttribute("src", vObj);
     audio.setAttribute("src", vObj);
@@ -32,16 +53,28 @@ function getNftObject() {
 
     var arg = GetQueryString();
 
-    var nft = document.getElementById("ar-gltf-main");
+    var nft = document.getElementById('ar-gltf-main');
 
-    var nObj = !(arg["o"]) ? '' : path + 'article/nftobject/' + arg["o"] + ".gltf";
-    nft.setAttribute("gltf-model", nObj);
+    if ((arg["o1"]) && (arg["o2"])) {
+        nObj = path + 'article/nftobject/' + arg["o1"] + '/' + arg["o2"] + '.gltf';
+    } else {
+        nObj = !(arg["o"]) ? '' : path + 'article/nftobject/' + arg["o"] + '.gltf';
+    }
+
+    nft.setAttribute('gltf-model', nObj);
 
     if(!!(arg["xs"]))
     {
-        var shodow = document.getElementById("ar-gltf-shadow");
-        var sObj = !(arg["o"]) ? '' : path + 'article/nftobject/' + arg["o"] + "-hs.gltf";
-        shodow.setAttribute("gltf-model-shadow", sObj);
+        var shodow = document.getElementById('ar-gltf-shadow');
+        var sObj;
+
+        if ((arg["o1"]) && (arg["o2"])) {
+            sObj = path + 'article/nftobject/' + arg["o1"] + '/' + arg["o2"] + '-hs.gltf';;
+        } else {
+            sObj = !(arg["o"]) ? '' : path + 'article/nftobject/' + arg["o"] + '-hs.gltf';
+        }
+
+        shodow.setAttribute('gltf-model-shadow', sObj);
 
         shodow.style.visibility = 'visible';
     }
@@ -51,7 +84,7 @@ function getNftMarker() {
     
     var arg = GetQueryString();
 
-    var nft = document.getElementById("ar-nft");
+    var nft = document.getElementById('ar-nft');
     var nObj;
 
     if ((arg["m1"]) && (arg["m2"])) {
@@ -60,5 +93,5 @@ function getNftMarker() {
         nObj = !(arg["m"]) ? '' : path + 'ImageDescriptors/' + arg["m"] + '/' + arg["m"];
     }
 
-    nft.setAttribute("url", nObj);
+    nft.setAttribute('url', nObj);
 };
