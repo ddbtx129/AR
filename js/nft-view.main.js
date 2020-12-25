b914ebc0a1499a211ebef168f4ec5742b35b0aaa
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-26 00:01:37 +0900

    Add files via upload

--- js/nft-view.main.js
+++ js/nft-view.main.js
@@ -5,9 +5,9 @@ var nftarView;
 
     var rootPath = "https://ddbtx129.github.io/AR/";
 
-    var scene = document.getElementById('ar-scene');
-    var marker = document.getElementById('ar-nft');
-    var nft = document.getElementById("ar-gltf-main");
+    //var scene = document.getElementById('ar-scene');
+    //var marker = document.getElementById('ar-nft');
+    //var nft = document.getElementById("ar-gltf-main");
 
     //var wrapPos = nft.getAttribute('position');
     //var arPosX = !(arg["x"]) ? 0 : arg["x"];
