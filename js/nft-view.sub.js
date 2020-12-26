96ce6df40bdfee16b837fb28f03095b964f0f04e
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-26 13:33:45 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -11,7 +11,7 @@
 			window.alert(3);
 			this.setArg();
 
-			//if (true) {
+			if (this.setArData()) {
 
 			//	var deviceevents = {
 			//        touch: typeof document.ontouchstart !== 'undefined',
@@ -25,7 +25,7 @@
 			//        end: deviceevents.pointer ? 'pointerup' : deviceevents.mspointer ? 'mspointerup' : deviceevents.touch ? 'touchend' : 'mouseup'
 			//    };
 
-			//}
+			}
 		},
 
 		setArg: function () {
@@ -38,39 +38,108 @@
 			for (var i = 0; pair[i]; i++) {
 				var kv = pair[i].split('=');
 				arg[kv[0]] = decodeURIComponent(kv[1]);
-				window.alert(arg[kv[0]]);
 			}
 
 			// 影
 			arg.shodowList = arg.xs && (parseInt(arg.xs, 16).toString(2));
-			window.alert(arg.shodowList);
 			// サイズ
 			arg.sizeList = arg.wh && (parseInt(arg.wh, 16).toString(10)).match(/.{2}/g);
-			window.alert(arg.sizeList);
 			// マーカー
 			arg.markerList = arg.m;
-			window.alert(arg.markerList);
 			arg.markerList1 = arg.m1;
-			window.alert(arg.markerList1);
 			arg.markerList2 = arg.m2;
-			window.alert(arg.markerList2);
 
 			// ar-gltf-main
 			arg.ObjectList = arg.o;
-			window.alert(arg.ObjectList);
 			arg.ObjectList1 = arg.o1;
-			window.alert(arg.ObjectList1);
 			arg.ObjectList2 = arg.o2;
-			window.alert(arg.ObjectList2);
 
 			self.arg = arg;
-		}
+		},
 
+		setArData: function () {
+			window.alert(5);
+
+		    var self = this;
+		    var arData = null;
+
+		    dataObj.isObject = !self.arg.ObjectList && self.arg.ObjectList1;
+		    window.alert(6);
+
+		    // データの準備
+		    var dataObj = {
+		        path: function () {
+		            if (dataObj.isObject) {
+		                return (rootPath + 'article/nftobject/' + self.arg.o1 + '/' + s.arg.o2 + '.gltf');
+		            } else {
+		            	return ((!self.arg.o) ? '' : self.arg.o + 'article/nftobject/' + self.arg.o + '.gltf');
+		            }
+		        }
+		    };
+		    window.alert(7);
+
+		    if (!arData.path) {
+		        // 画像なかった
+		        Err_Exit('画像情報が取得できませんでした。');
+		        return false;
+		    } else {
+		    	window.alert(8);
+
+		        var marker = document.getElementById('ar-nft');
+		        var nft = document.getElementById("ar-gltf-main");
+
+		        nft.setAttribute('gltf-model', AFRAME.utils.coordinates.stringify(dataObj.path));
+
+		        dataObj.isMarker = !!self.arg.markerList;
+		        dataObj.isMarker12 = !!self.arg.markerList1 && !!self.arg.markerList2;
+
+		        if (dataObj.isMarker12) {
+		        	marker.setAttribute('url',
+                        AFRAME.utils.coordinates.stringify(
+                            rootPath + 'ImageDescriptors/' + self.arg.m1 + '/' + self.arg.m2));
+		        } else {
+		        	marker.setAttribute('url',
+                        AFRAME.utils.coordinates.stringify(
+                            !(sel.arg.isMarker) ? '' : path + 'ImageDescriptors/' + self.arg.m + '/' + self.arg.m));
+		        }
+		        window.alert(9);
+
+		        dataObj.isShadow = self.arg.shodowList && !!Number(self.arg.shodowList);
+
+		        var wh = JoinNum(String(self.arg.sizeList ? self.arg.sizeList : '1010').split(''), 2);
+		        dataObj.size = { w: wh[0], y: wh[1], z: wh[0] };
+		        window.alert(10);
+
+		        dataObj.posVec3 = self.positionVec3('main');
+
+		        nft.setAttribute('scale', AFRAME.utils.coordinates.stringify(dataObj.size));
+		        nft.setAttribute('position', AFRAME.utils.coordinates.stringify(posVec3));
+		        nft.setAttribute('rotation', '0 0 0');
+		    }
+
+		    arData = dataObj;
+		    self.arData = arData;
+
+		    return true;
+		},
+
+		JoinNum: function (val, s) {
+			var k = val.length / s;
+			var rtn = {};
+
+			for (var i = 0; i < s; i++) {
+				for (var j = 0; j < k; j++) {
+					rtn[i] += String(val[i + j]);
+				}
+			}
+
+			return rtn;
+        }
 	};
 
 	window.alert(1);
-	
 	webArNft.nft = nft;
+
 	window.alert(2);
 	webArNft.nft.nftInit();
 
