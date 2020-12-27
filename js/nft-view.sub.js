3804131932f92763a1a7339f1962cbd1f25617c6
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-28 01:36:17 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -108,6 +108,7 @@ var zoomH = 0;
 		        var nft = document.getElementById("ar-gltf-main");
 
 		        nft.setAttribute('gltf-model', AFRAME.utils.coordinates.stringify(dataObj.path));
+				nft.style.zIndex = 9999;
 
 		        if ((!!self.arg.markerList1) && (!!self.arg.markerList2)) {
 		        	marker.setAttribute('url',
@@ -148,9 +149,10 @@ var zoomH = 0;
 					};
 
 					nftShadow.setAttribute('gltf-model', AFRAME.utils.coordinates.stringify(dataObj.shadow.path));
+					nft.style.zIndex = 9899;
 
 					nftShadow.setAttribute('scale', String(dataObj.size.w) + ' ' + String(dataObj.size.h) + ' ' + String(dataObj.size.w));
-					nftShadow.setAttribute('position', String(dataObj.posVec3.x) + ' ' + String(dataObj.posVec3.y - (dataObj.size.h / 2)) + ' ' + String(dataObj.posVec3.z));
+					nftShadow.setAttribute('position', String(dataObj.posVec3.x) + ' ' + String(dataObj.posVec3.y) + ' ' + String(dataObj.posVec3.z - (dataObj.size.h / 2)));
 					nftShadow.setAttribute('rotation', '-90 0 0');
 
 					//dataObj.shadow = shadow;
