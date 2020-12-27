7f79f0674d35fac02b2d6651c41de0386a35e64b
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-28 01:24:16 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -140,18 +140,20 @@ var zoomH = 0;
 
 					nftShadow.style.visibility = "visible";
 
-					var shadow = {
+					dataObj.shadow = {
 						path: (!(self.arg.ObjectList) ?
 							(rootPath + 'article/nftobject/' + self.arg.ObjectList1 + '/' + self.arg.ObjectList2 + '-xs.gltf')
 							:
 							(!(self.arg.ObjectList) ? '' : rootPath + 'article/nftobject/' + self.arg.ObjectList + '-xs.gltf'))
 					};
-					
+
+					nftShadow.setAttribute('gltf-model', AFRAME.utils.coordinates.stringify(dataObj.shadow.path));
+
 					nftShadow.setAttribute('scale', String(dataObj.size.w) + ' ' + String(dataObj.size.h) + ' ' + String(dataObj.size.w));
 					nftShadow.setAttribute('position', String(dataObj.posVec3.x) + ' ' + String(dataObj.posVec3.y) + ' ' + String(dataObj.posVec3.z));
 					nftShadow.setAttribute('rotation', '-90 0 0');
 
-					dataObj.shadow = shadow;
+					//dataObj.shadow = shadow;
 				}
 		    }
 
