a38ab8d213da4308a47ea8844dff0abebd7a7911
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-28 21:42:06 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -150,18 +150,18 @@ var zoomH = 0;
 					var shadow = document.createElement('a-entity');
 
 					var shadowPosVec3 = this.positionVec3('shadow', dataObj.size.h);
-					window.alert(shadowPosVec3.z);
-					shadow.setAttribute('position', String(shadowPosVec3.x) + ' ' + String(shadowPosVec3.y) + ' ' + String(shadowPosVec3.z));
+					window.alert(shadowPosVec3.x);
+					shadow.setAttribute('position', String(shadowPosVec3.x) + ' ' + String(shadowPosVec3.y) + ' ' + String(dashadowPosVec3.z));
 					shadow.setAttribute('rotation', '-90 0 0');
 
-					//AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', {
-					//	primitive: 'plane', height: dataObj.size.h, width: dataObj.size.w
-					//});
+					AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', {
+						primitive: 'box', scale: String(dataObj.size.w) + ' ' + String(dataObj.size.h) + ' ' + String(dataObj.size.w)
+					});
 
-					//AFRAME.utils.entity.setComponentProperty(shadow, 'material', {
-					//	shader: 'flat', npot: true, src: "#arGltf-main", transparent: true, alphaTest: 0.1,
-					//	color: 'black', opacity: 0.3, depthTest: false
-					//});
+					AFRAME.utils.entity.setComponentProperty(shadow, 'material', {
+						shader: 'flat', npot: true, src: "#arGltf-main", transparent: true, alphaTest: 0.1,
+						color: 'black', opacity: 0.3, depthTest: false
+					});
 
 					dataObj.shadow = shadow;
 					nft.appendChild(dataObj.shadow);
