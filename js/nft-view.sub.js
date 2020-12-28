3356da3d030f3af795d29e3902ad4adae3af64ec
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-28 22:13:02 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -157,13 +157,11 @@ var zoomH = 0;
 					};
 
 					var shadowPosVec3 = this.positionVec3('shadow', dataObj.size.h);
-					window.alert(shadowPosVec3.z);
+					window.alert(shadowPosVec3.x);
 					shadow.setAttribute('position', String(shadowPosVec3.x) + ' ' + String(shadowPosVec3.y) + ' ' + String(dashadowPosVec3.z));
 					shadow.setAttribute('rotation', '-90 0 0');
 
-					AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', {
-						primitive: 'plane', height: dataObj.size.h, width: dataObj.size.w
-					});
+					AFRAME.utils.entity.setComponentProperty(shadow, 'geometry', { primitive: 'plane' });
 
 					//AFRAME.utils.entity.setComponentProperty(shadow, 'material', {
 					//	shader: 'flat', npot: true, src: "#arGltf-main", transparent: true, alphaTest: 0.1,
