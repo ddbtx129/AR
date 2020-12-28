f2be4f1befab05080c17191dab7054761a1d9da1
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-28 18:25:32 +0900

    Add files via upload

--- js/nft-view.sub.js
+++ js/nft-view.sub.js
@@ -206,7 +206,7 @@ var zoomH = 0;
 		},
 
 		setSwitcher: function () {
-			window.alert(113);
+			window.alert(115);
 
 			var scene = document.getElementById('arScene');
 			var nft = document.getElementById("arGltf-main");
@@ -230,10 +230,9 @@ var zoomH = 0;
 			scene.addEventListener(self.eventnames.move, function (e) {
                 var event = e.changedTouches ? e.changedTouches[0] : e;
                 if (prevPageY) {
-					if ((zoomH + (((prevPageY - event.pageY) * 10) / scene.clientHeight / 5))> 0.1) {
-						zoomW += (((prevPageY - event.pageY) * 10) / scene.clientHeight / 5);
-						zoomH += (((prevPageY - event.pageY) * 10) / scene.clientHeight / 5);
-						window.alert(zoomH);
+					if (((prevPageY - event.pageY) / scene.clientHeight / 5) * 48 > 0.1) {
+						zoomW += (prevPageY - event.pageY) / scene.clientHeight / 5;
+						zoomH += ((prevPageY - event.pageY) / scene.clientHeight / 5) * 48;
 						AFRAME.utils.entity.setComponentProperty(nft, 'animation__scale', {
 							property: 'scale', dur: 5, easing: 'linear', loop: false, to: zoomH + ' ' + zoomH + ' ' + zoomH
 						});
