7b7e0fed83151a39077e820c9b0ebbb9d7495d31
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-30 21:11:18 +0900

    Add files via upload

---
+++ js/video.object.js
@@ -0,0 +1,100 @@
+﻿var path = "https://ddbtx129.github.io/AR/";
+var arg = GetQueryString();
+
+function getArVideo() {
+
+    //var arg = GetQueryString();
+
+    var video = document.getElementById("arVideo");
+    //var vObj = !(arg["o"]) ? 'article/video/notfound_video.mp4' : 'article/video/' + arg["o"] + '.mp4';
+    var vObj;
+
+    if ((arg["o1"]) && (arg["o2"])) {
+        vObj = 'article/video/' + arg["o1"] + '/' + arg["o2"] + '.mp4';
+    } else {
+        vObj = !(arg["o"]) ? 'article/video/notfound_video.mp4' : 'article/video/' + arg["o"] + '.mp4';
+    }
+
+    video.setAttribute("src", vObj);
+};
+
+function getArMarker() {
+
+    var marker = document.getElementById("arMarker");
+    //var mObj = !(arg["m"]) ? 'pattern/pattern-def.patt' : 'pattern/pattern-' + arg["m"] + '.patt';
+    var mObj;
+
+    if ((arg["m1"]) && (arg["m2"])) {
+        mObj = path + 'pattern/' + arg["m1"] + '/pattern-' + arg["m2"] + '.patt';
+    } else {
+        mObj = !(arg["m"]) ? 'pattern/pattern-def.patt' : 'pattern/pattern-' + arg["m"] + '.patt';
+    }
+
+    marker.setAttribute("url", mObj);
+};
+
+function getNftVideo() {
+    
+    //var arg = GetQueryString();
+
+    var video = document.getElementById("arVideo");
+    var audio = document.getElementById("arAudio");
+    
+    //var vObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
+    var vObj;
+
+    if ((arg["o1"]) && (arg["o2"])) {
+        vObj = path + 'article/video/' + arg["o1"] + '/' + arg["o2"] + '.mp4';
+    } else {
+        vObj = !(arg["o"]) ? path + 'article/video/notfound_video.mp4' : path + 'article/video/' + arg["o"] + '.mp4';
+    }
+
+    video.setAttribute("src", vObj);
+    audio.setAttribute("src", vObj);
+};
+
+function getNftObject() {
+
+    //var arg = GetQueryString();
+
+    var main = document.getElementById('arMain');
+
+    if ((arg["o1"]) && (arg["o2"])) {
+        nObj = path + 'article/gltf/' + arg["o1"] + '/' + arg["o2"] + '.gltf';
+    } else {
+        nObj = !(arg["o"]) ? '' : path + 'article/gltf/' + arg["o"] + '.gltf';
+    }
+
+	main.setAttribute('gltf-model', nObj);
+
+    if(!!(arg["xs"]))
+    {
+        var shodow = document.getElementById('arGltf-shadow');
+        var sObj;
+
+        if ((arg["o1"]) && (arg["o2"])) {
+            sObj = path + 'article/gltf/' + arg["o1"] + '/' + arg["o2"] + '-hs.gltf';;
+        } else {
+            sObj = !(arg["o"]) ? '' : path + 'article/gltf/' + arg["o"] + '-hs.gltf';
+        }
+
+        shodow.setAttribute('gltf-model-shadow', sObj);
+        shodow.style.visibility = 'visible';
+    }
+};
+
+function getNftMarker() {
+    
+    var arg = GetQueryString();
+
+    var nft = document.getElementById('arNft');
+    var nObj;
+
+    if ((arg["m1"]) && (arg["m2"])) {
+        nObj = path + 'ImageDescriptors/' + arg["m1"] + '/' + arg["m2"] + '/' + arg["m2"];
+    } else {
+        nObj = !(arg["m"]) ? '' : path + 'ImageDescriptors/' + arg["m"] + '/' + arg["m"];
+    }
+
+    nft.setAttribute('url', nObj);
+};
