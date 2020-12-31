44af09cb9cada123c40852e4bb4d981e3275eb45
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2020-12-31 14:13:37 +0900

    Add files via upload

--- js/nft-view.main.js
+++ js/nft-view.main.js
@@ -131,7 +131,7 @@ var zoomH = 0;
 
 				nft.setAttribute('scale', String(dataObj.size.w) + ' ' + String(dataObj.size.h) + ' ' + String(dataObj.size.w));
 				nft.setAttribute('position', String(dataObj.posVec3.x) + ' ' + String(dataObj.posVec3.y) + ' ' + String(dataObj.posVec3.z));
-				nft.setAttribute('rotation', '90 0 0');
+				nft.setAttribute('rotation', '0 0 0');
 
 				dataObj.isShadow = self.arg.shodowList && !!Number(self.arg.shodowList);
 
@@ -202,7 +202,7 @@ var zoomH = 0;
 
 			bParallel.addEventListener('click', function () {
 				if (!bParallel.classList.contains('current')) {
-					nft.setAttribute('rotation', AFRAME.utils.coordinates.stringify('90 0 0'));
+					nft.setAttribute('rotation', AFRAME.utils.coordinates.stringify('0 0 0'));
 					nft.setAttribute('position', String(defaultPos.x) + ' ' + String(defaultPos.y) + ' ' + String(defaultPos.z));
 					bParallel.classList.add('current');
 					bAngle.classList.remove('current');
