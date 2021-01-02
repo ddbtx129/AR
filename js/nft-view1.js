fda7b890541707b4ecd236cac7fd7662c178f9cb
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-02 13:35:17 +0900

    Update nft-view1.js

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -406,8 +406,8 @@ var zoomH = 0;
             if (type === 'shadow') {
                 return { x: 0, y: 0, z: -h1_2 };
             } else {
-                //return { x: 0, y: h1_2, z: 0 };
-                return { x: 10, y: 0, z: 0 };
+                return { x: 0, y: h1_2, z: 0 };
+                //return { x: 10, y: 0, z: 0 };
             }
         }
     };
