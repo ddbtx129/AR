f587e87f43a2f570568e33392b9a7041be1b7fb4
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-03 17:54:47 +0900

    Add files via upload

--- js/nft-view2.js
+++ js/nft-view2.js
@@ -149,7 +149,6 @@ var videoState = 0;
                 }
                 else if (dataObj.isMp4) {
 
-                    document.getElementById('info1').style.display = "inline";
                     document.body.style.display = 'table-cell';
 
                     window.alert(18);
@@ -297,6 +296,8 @@ var videoState = 0;
                     main.setAttribute('height', AFRAME.utils.coordinates.stringify(val.size.h));
                     main.setAttribute('play', 'true');
                     window.alert(26.5);
+
+                    document.getElementById('info1').style.display = "inline";
                 } 
 
                 window.alert(27);
