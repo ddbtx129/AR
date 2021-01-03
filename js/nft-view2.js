5517ff8735d3004d27f7488cbdfe867714b847a2
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-03 20:29:36 +0900

    Add files via upload

--- js/nft-view2.js
+++ js/nft-view2.js
@@ -340,7 +340,7 @@ var videoState = 0;
 
             // NFTマーカー
             var mWrap = document.createElement('a-nft');
-
+            mWrap.setAttribute('videohandler', ''); 
             mWrap.setAttribute('preset', 'custom');
             mWrap.setAttribute('type', 'nft');
             mWrap.setAttribute('id', 'arMarker');
@@ -590,3 +590,54 @@ var videoState = 0;
     webArViewer.ar.init();
 
 }());
+
+AFRAME.registerComponent('videohandler', {
+    init: function () {
+
+        // ビデオ格納用の変数定義を追加
+        var video = document.querySelector('#source');
+        var marker = this.el;
+
+        // マーカーを検出したイベントの登録
+        marker.addEventListener('markerFound', function () {
+
+            if (videostate == 0) {
+                document.getElementById("player").style.display = 'inline';
+            }
+
+            // マーカー認識したら、ビデオ再生
+            video.play();
+            videostate = 1;
+        });
+
+        // マーカーを見失ったイベントの登録
+        marker.addEventListener('markerLost', function () {
+
+            // マーカー認識が外れたら、、ビデオ停止
+            video.pause();
+            videostate = 2;
+        });
+    }
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var btn = document.getElementById('player');
+
+    btn.addEventListener('click', function () {
+
+        if (videostate >= 1 && videostate < 2) {
+
+            var video = document.querySelector('#source');
+            video.play();
+
+            videostate = 1;
+
+            // プレインボタン 非表示
+            document.getElementById("player").style.display = 'none';
+
+            p1.style.display = "none";
+            info1.style.display = "none";
+        }
+    });
+});
