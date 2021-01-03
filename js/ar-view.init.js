32a2bf1b56a16beb2703276778973449f0415431
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-03 16:59:21 +0900

    Add files via upload

--- js/ar-view.init.js
+++ js/ar-view.init.js
@@ -1,5 +1,5 @@
 ﻿
-window.addEventListener('DOMContentLoaded', function () {
+(function () {
 
     var arg = {};
     var pair = location.search.substring(1).split('&');
@@ -11,35 +11,41 @@ window.addEventListener('DOMContentLoaded', function () {
 
     var isMp4 = !!(dataObj.path || '').match(/\.mp4$/i);
 
-    var btn = document.getElementById('player');
-
-
+    window.alert(110);
     if (isMp4) {
+        window.alert(111);
 
-        var videoinfo = document.createElement("div");
-        videoinfo.setAttribute('id', 'info1');
-        videoinfo.setAttribute('info1', 'info1');
+        var divEl = document.createElement("div");
+        divEl.setAttribute('id', 'info1');
+        divEl.setAttribute('info1', 'info1');
+        window.alert(112);
 
-        var videotag = document.createElement("a");
-        videotag.setAttribute('id', 'swPlay');
-        videotag.setAttribute('info1', 'InfoItem');
+        var aEl = document.createElement("a");
+        aEl.setAttribute('id', 'swPlay');
+        aEl.setAttribute('info1', 'InfoItem');
+        window.alert(113);
 
-        var videotext = document.createElement("p");
-        videotext.setAttribute('id', 'p1');
+        var pEl = document.createElement("p");
+        pEl.setAttribute('id', 'p1');
         var pbr = document.createElement('br')
-        var ptext1 = document.createTextNode('画面をタップすると、再生が開始されます。\n\n音声が流れますのでご注意下さい。');
+        var pText1 = document.createTextNode('画面をタップすると、再生が開始されます。\n\n音声が流れますのでご注意下さい。');
+        window.alert(114);
 
-        var play = document.createElement("img");
-        play.setAttribute('id', 'player');
-        play.setAttribute('class', 'playbutton');
-        play.setAttribute('src', 'asset/play.png');
-        play.style.display = 'inline';
+        var bPlay = document.createElement("img");
+        bPlay.setAttribute('id', 'player');
+        bPlay.setAttribute('class', 'playbutton');
+        bPlay.setAttribute('src', 'asset/play.png');
+        bPlay.style.display = 'inline';
+        window.alert(115);
 
-        videotext.appendChild(ptext1);
-        videotag.appendChild(videotext);
-        videoinfo.appendChild(videotag);
+        pEl.appendChild(pText1);
+        aEl.appendChild(videotext);
+        divEl.appendChild(aEl);
+        window.alert(116);
 
-        document.body.appendChild(videoinfo);
-        document.body.appendChild(play);
+        document.body.appendChild(divEl);
+        document.body.appendChild(plbPlayay);
+        window.alert(117);
     }
-});
+
+}());
