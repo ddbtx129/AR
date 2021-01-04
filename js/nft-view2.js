8daa08b95caf442f4db9ba538ff4546a85d6792f
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-04 13:20:22 +0900

    Add files via upload

--- js/nft-view2.js
+++ js/nft-view2.js
@@ -45,7 +45,6 @@ var SizeRate = 10;
                     objecttype = "pic";
                     document.getElementById("info1").style.display = "none";
                 } else {
-                    //document.body.style.display = "table-cell;";
                     objecttype = "video";
                 }
             }
