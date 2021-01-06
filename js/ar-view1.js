e63e122606e99c079bcc48cf258e0b4585b09225
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-06 15:54:20 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -47,7 +47,11 @@ var SizeRate = 10;
 
                 if (!this.arData.isMp4) {
                     objecttype = "pic";
-
+                    window.al(0);
+                    document.getElementById("modeSwitch").style.display = "inline";
+                    if (this.arg.preview) {
+                        document.getElementById("swCamera").style.display = "inline";
+                    }
                 } else {
                     objecttype = "video";
                     //document.getElementById("info1").style.display = "inline";
@@ -238,21 +242,13 @@ var SizeRate = 10;
 
             swMarker.addEventListener('click', function () {
                 if (!this.classList.contains('current')) {
-                    location.replace(location.search.replace('&preview=1', '')); d
-                    ocument.getElementById("modeSwitch").style.display = "inline";
-                    if (this.arg.preview) {
-                        document.getElementById("swCamera").style.display = "inline";
-                    }
+                    location.replace(location.search.replace('&preview=1', '')); 
                 }
             });
 
             swPreview.addEventListener('click', function () {
                 if (!this.classList.contains('current')) {
                     location.replace(location.search + '&preview=1');
-                    ocument.getElementById("modeSwitch").style.display = "inline";
-                    if (this.arg.preview) {
-                        document.getElementById("swCamera").style.display = "inline";
-                    }
                 }
             });
         },
