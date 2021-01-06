bf132501681ef67cecacd4742ffc5b20273d82c8
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-06 19:07:47 +0900

    Update ar-view1.js

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -553,7 +553,7 @@ var SizeRate = 10;
 
         setDiplayBtn: function()
         {
-            window.alret('-1');
+            window.alert('-1');
             var self = this;
             document.getElementById("swUp").style.display = 'inline';
             document.getElementById("swDown").style.display = 'inline';
