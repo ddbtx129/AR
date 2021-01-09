96605b5147cd9270433c2a2be8febf3aff48d46e
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-10 03:25:44 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -378,7 +378,7 @@ var SizeRate = 10;
             if (val.isLogo) {
                 var logo = document.createElement('a-entity');
                 logo.setAttribute('id', 'logo');
-                logo.setAttribute('position', '-4 -4 -4');
+                logo.setAttribute('position', '2 -2 0');
                 logo.setAttribute('rotation', '-10 0 0');
                 logo.setAttribute('scale', '10 10 10');
                 logo.setAttribute('gltf-model', '#logosource');
