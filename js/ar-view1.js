0b26db99bfa1a96cfc9257a4aa0d2bf42ac7a4da
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-10 21:46:07 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -402,6 +402,8 @@ var SizeRate = 10;
                     //});
                 //}
 
+                logo.setAttribute('radius', '1.25');
+
                 AFRAME.utils.entity.setComponentProperty(logo, 'animation', {
                     property: 'rotation', dur: 3000, loop: true, easing: 'easeOutElastic', elasticity: 300, from: '-5 0 0', to: '-5 0 360' });
 
