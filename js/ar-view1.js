8904483f01df8344fcb8d041bbf2300811c4d025
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-10 11:49:16 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -376,7 +376,7 @@ var SizeRate = 10;
             if (val.isLogo) {
                 var logo = document.createElement('a-entity');
                 logo.setAttribute('id', 'logo');
-                logo.setAttribute('position', AFRAME.utils.coordinates.stringify(((val.isPV) ? positionVec3(-1, -1, -1.5) : positionVec3(-1, -2, 0))));
+                logo.setAttribute('position', AFRAME.utils.coordinates.stringify(((val.isPV) ? posAttribute(-1, -1, -1.5) : posAttribute(-1, -2, 0))));
                 logo.setAttribute('rotation', AFRAME.utils.coordinates.stringify('-5 0 0'));
                 logo.setAttribute('scale', AFRAME.utils.coordinates.stringify((val.isPV) ? '8, 8, 8' : '25, 25 25'));
                 logo.setAttribute('gltf-model', '#logosource');
@@ -642,7 +642,7 @@ var SizeRate = 10;
             }
         },
 
-        positionVec3: function (px, py, pz) {
+        posAttribute: function (px, py, pz) {
             var self = this; return { x: px, y: py, z: pz };
         },
 
