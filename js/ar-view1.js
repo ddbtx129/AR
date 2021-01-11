ac21a42fbcd1029237c3702ef8ed35acfc3b8b4f
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-11 19:35:12 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -534,14 +534,14 @@ var SizeRate = 10;
                         self.wrap.setAttribute('rotation', AFRAME.utils.coordinates.stringify(arRotation));
                         self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(wrapPos));
                         if (val.isLogo) {
-                            self.arData.logo.setAttribute('rotation', AFRAME.utils.coordinates.stringify(arRotation));
+                            //self.arData.logo.setAttribute('rotation', AFRAME.utils.coordinates.stringify(arRotation));
                             if (val.isTurn == 1) {
                                 AFRAME.utils.entity.setComponentProperty(logo, 'animation', {
-                                    property: 'rotation', from: '-90 0 180', to: '-90 0 360', dur: 3000, loop: true, easing: 'linear'
+                                    property: 'rotation', from: '0 0 180', to: '0 0 360', dur: 3000, loop: true, easing: 'linear'
                                 });
                             } else if (val.isTurn == 2) {
                                 AFRAME.utils.entity.setComponentProperty(logo, 'animation', {
-                                    property: 'rotation', from: '-90 0 0', to: '-90 0 360', dur: 3000, loop: true, easing: 'easeOutElastic', elasticity: 300
+                                    property: 'rotation', from: '0 0 0', to: '0 0 360', dur: 3000, loop: true, easing: 'easeOutElastic', elasticity: 300
                                 });
                             }
                             self.arData.logo.setAttribute('position', AFRAME.utils.coordinates.stringify(self.positionVec3Logo('p')));
