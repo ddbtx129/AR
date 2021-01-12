134f39fd407325b78d5d0e70ef39921e65a41302
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-12 23:17:03 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -774,12 +774,11 @@ var SizeRate = 10;
 
         positionVec3: function (type) {
             var self = this;
-            //var h1_2 = (self.arData.size.h / 2);
             var h1_2 = (self.arData.size.h / 2);
 
-            //if (self.arData.size.w > self.arData.size.h) {
-            //    h1_2 = (self.arData.size.w / 2);
-            //}
+            if (self.arData.size.w > self.arData.size.h) {
+                h1_2 = (self.arData.size.w / 2);
+            }
 
             if (type === 'shadow') {
                 return { x: 0, y: 0, z: -h1_2 };
