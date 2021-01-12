4a7624efef093b2d22a3ca19f131c2aefdd398b1
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-12 17:05:50 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -409,39 +409,39 @@ var SizeRate = 10;
                 //    side: 'double', transparent: true, alphaTest: 0.1, metalness: (!!(val.isReflect) ? 1 : 0), roughness: (!!(val.isReflect) ? 0.3 : 0.5)
                 //});
 
-                AFRAME.utils.entity.setComponentProperty(logo, 'animation__pos', {
-                    property: 'position', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: logopos + ' ' + (logopos + logoscale / 3) + ' ' + logopos
-                });
+                //AFRAME.utils.entity.setComponentProperty(logo, 'animation__pos', {
+                //    property: 'position', dir: 'alternate', dur: 400, easing: 'easeInOutQuart', loop: true, to: logopos + ' ' + (logopos + logoscale / 3) + ' ' + logopos
+                //});
 
-                AFRAME.utils.entity.setComponentProperty(logo, 'animation__scale', {
-                    property: 'scale', dir: 'alternate', dur: 400, easing: 'easeOutQuad', loop: true, to: '0.94 1.06 1'
-                });
+                //AFRAME.utils.entity.setComponentProperty(logo, 'animation__scale', {
+                //    property: 'scale', dir: 'alternate', dur: 400, easing: 'easeOutQuad', loop: true, to: '0.94 1.06 1'
+                //});
 
-                //if (!!val.isTurn) {
-                //    logo.setAttribute('radius', logoscale);
-                //    if (val.isTurn == 1) {
-                //        AFRAME.utils.entity.setComponentProperty(logo, 'animation__turn', {
-                //            property: 'rotation',
-                //            from: String(objAngle) + ' 0 0',
-                //            to: String(objAngle) + ' 360 0',
-                //            dur: 3000,
-                //            loop: true,
-                //            easing: 'linear'
-                //        });
-                //    } else if (val.isTurn == 2) {
-                //        AFRAME.utils.entity.setComponentProperty(logo, 'animation__turn', {
-                //            property: 'rotation',
-                //            from: String(objAngle) + ' 0 0',
-                //            to: String(objAngle) + ' 360 0',
-                //            dur: 3000,
-                //            loop: true,
-                //            easing: 'easeOutElastic',
-                //            elasticity: 300
-                //        });
-                //    } else {
+                if (!!val.isTurn) {
+                    logo.setAttribute('radius', logoscale);
+                    if (val.isTurn == 1) {
+                        AFRAME.utils.entity.setComponentProperty(logo, 'animation__turn', {
+                            property: 'rotation',
+                            from: String(objAngle) + ' 0 0',
+                            to: String(objAngle) + ' 360 0',
+                            dur: 3000,
+                            loop: true,
+                            easing: 'linear'
+                        });
+                    } else if (val.isTurn == 2) {
+                        AFRAME.utils.entity.setComponentProperty(logo, 'animation__turn', {
+                            property: 'rotation',
+                            from: String(objAngle) + ' 0 0',
+                            to: String(objAngle) + ' 360 0',
+                            dur: 3000,
+                            loop: true,
+                            easing: 'easeOutElastic',
+                            elasticity: 300
+                        });
+                    } else {
                         logo.setAttribute('rotation', AFRAME.utils.coordinates.stringify(String(objAngle) + ' 0 0'));
-                //    }
-                //}
+                    }
+                }
 
                 self.arData.logo = logo;
             }
