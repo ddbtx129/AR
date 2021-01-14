2263ada5cd4051e88e93d71b528ce964b233b1db
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-15 01:22:03 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -467,6 +467,10 @@ var SizeRate = 20;
             self.arData.shadow && self.wrap.appendChild(self.arData.shadow);
             self.arData.main && self.wrap.appendChild(self.arData.main);
 
+            if (val.isLogo) {
+                self.arData.logo && self.wrap.appendChild(self.arData.logo);
+            }
+
             if (!val.isMp4) {
                 document.getElementById("player").style.display = 'none';
             }
@@ -523,10 +527,6 @@ var SizeRate = 20;
             mWrap.setAttribute('url', AFRAME.utils.coordinates.stringify(rootPath + mk));
             
             mWrap.appendChild(self.wrap);
-
-            if (val.isLogo) {
-                self.arData.logo && mWrap.appendChild(self.arData.logo);
-            }
 
             webArViewer.scene.appendChild(mWrap);
             self.mWrap = mWrap;
@@ -731,8 +731,9 @@ var SizeRate = 20;
                 h1_2 = (self.arData.size.w / 2);
             }
 
-            return { x: 0, y: -(h1_2) + 7.5 + posy, z: 0 };
+            //return { x: 0, y: -(h1_2) + 7.5 + posy, z: 0 };
             //return { x: 0, y: 0, z: 0 };
+            return { x: 0, y: h1_2, z: 0 };
         },
 
         positionVec3: function (type) {
