80073051aef0c41805151d578a79b165fb6a0050
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-15 14:23:00 +0900

    Update ar-view1.js

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -175,7 +175,7 @@ var SizeRate = 10;
             var i = ((parseInt(self.arg.sizeList).toString(10)).length % 2 == 0) ? (parseInt(self.arg.sizeList).toString(10)).length : (parseInt(self.arg.sizeList).toString(10)).length + 1;
             dataObj.size = { w: (Number(wh[0]) * (10 ** -((i - 2) / 2))).toFixed(1), h: (Number(wh[1]) * (10 ** -((i - 2) / 2))).toFixed(1) };
             defaultSize = { w: (Number(wh[0]) * (10 ** -((i - 2) / 2))).toFixed(1), h: (Number(wh[1]) * (10 ** -((i - 2) / 2))).toFixed(1) };
-
+window.alert(dataObj.size.w);
             if (dataObj.path) {
 
                 var folder = !!(dataObj.isMp4) ? 'video' : (!!(dataObj.isGltf) ? 'gltf' : 'pic');
