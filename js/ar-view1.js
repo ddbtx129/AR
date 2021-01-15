73fb56aa21284a94dfa9c4d821b5eb36e992d69f
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-15 14:44:49 +0900

    Add files via upload

--- js/ar-view1.js
+++ js/ar-view1.js
@@ -170,13 +170,12 @@ var SizeRate = 10;
             defaultAngle = (self.arg.angleList && Number(self.arg.angleList) == 1) ? -90 : 0;
             
             //var wh = (String(!!(self.arg.sizeList) ? self.arg.sizeList : '20,20')).split(',');
-            self.arg.sizeList = (String(!!(self.arg.sizeList) && Number(self.arg.ar) == 0) ? SizeSplit(self.arg.sizeList) : SizeSplit(DefaultSize(self.arg.ar, objecttype)));
-            var wh = self.arg.sizeList.toString().split(',');
-            window.alert(self.arg.sizeList)
+            self.arg.sizeList = (String(!!(self.arg.sizeList) && Number(self.arg.ar) == 0) ? self.arg.sizeList : DefaultSize(self.arg.ar, objecttype));
+            var wh = SizeSplit(self.arg.sizeList.toString()).split(',');
             var i = ((parseInt(self.arg.sizeList).toString(10)).length % 2 == 0) ? (parseInt(self.arg.sizeList).toString(10)).length : (parseInt(self.arg.sizeList).toString(10)).length + 1;
             dataObj.size = { w: (Number(wh[0]) * (10 ** -((i - 2) / 2))).toFixed(1), h: (Number(wh[1]) * (10 ** -((i - 2) / 2))).toFixed(1) };
             defaultSize = { w: (Number(wh[0]) * (10 ** -((i - 2) / 2))).toFixed(1), h: (Number(wh[1]) * (10 ** -((i - 2) / 2))).toFixed(1) };
-window.alert(dataObj.size.w);
+            window.alert(dataObj.size.w);
             if (dataObj.path) {
 
                 var folder = !!(dataObj.isMp4) ? 'video' : (!!(dataObj.isGltf) ? 'gltf' : 'pic');
