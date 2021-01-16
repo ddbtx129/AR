7780428eb0954dd7c24042ebe14493e6f2f1e642
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-17 02:15:19 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -848,11 +848,12 @@ var SizeRate = 20;
             if (type === 'shadow') {
                 return { x: 0, y: -(self.arData.size.h), z: -h1_2 };
             } else {
-                if (angle == 1) {
-                    return { x: 0, y: 0, z: h1_2 * ((self.arData.isMarkerType == 1) ? -1 : 1) };
-                } else {
-                    return { x: 0, y: h1_2 * ((self.arData.isMarkerType == 1) ? -1 : 1), z: 0 };
-                }
+                //if (angle == 1) {
+                //    return { x: 0, y: 0, z: h1_2 * ((self.arData.isMarkerType == 1) ? -1 : 1) };
+                //} else {
+                //    return { x: 0, y: h1_2 * ((self.arData.isMarkerType == 1) ? -1 : 1), z: 0 };
+                //}
+                return { x: 0, y: h1_2 * ((self.arData.isMarkerType == 1) ? -1 : 1), z: 0 };
             }
         }
     };
