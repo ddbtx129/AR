9773c965a9ffdd63ca4e24959a8d46e34d9d8ee8
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-16 12:19:19 +0900

    Update nft-view1.js

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -170,7 +170,7 @@ var SizeRate = 20;
 
             dataObj.isShadow = self.arg.shodowList && !!Number(self.arg.shodowList);
             defaultAngle = (self.arg.angleList && Number(self.arg.angleList) == 1) ? -90 : -5;
-            
+            window.alert(defaultAngle);
             self.arg.sizeList = String(!!(!!(self.arg.sizeList) && Number(self.arg.ar) == 0) ? self.arg.sizeList : DefaultSize((dataObj.isMarkerType == 1 ? 1 : 0), objecttype));
             var wh = SizeSplit(self.arg.sizeList).toString().split(',');
             var i = ((parseInt(self.arg.sizeList).toString(10)).length % 2 == 0) ? (parseInt(self.arg.sizeList).toString(10)).length : (parseInt(self.arg.sizeList).toString(10)).length + 1;
