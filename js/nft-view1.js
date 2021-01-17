aff569cbd48c483cc0032adab44273cc1cd6d859
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-17 23:32:18 +0900

    Update nft-view1.js

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -535,7 +535,7 @@ var SizeRate = 20;
 
                 wrapPos.x -= 0;
                 wrapPos.y -= ((val.isMp4) ? 0 : 2);
-                wrapPos.z -= val.size.h * (!!(val.isMarkerType == 1) ? 1 : 2);
+                wrapPos.z -= val.size.h * (!!(val.isMarkerType == 1) ? 1 : 3);
 
                 var pvAngle = 0;
 
