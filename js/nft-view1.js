3194fc21f1c3aa0d0b702ecc1b3f305b3c028ccb
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-17 12:34:13 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -287,7 +287,7 @@ var SizeRate = 20;
 
             var self = this;
             var base = self.arg.base ? decodeURI(self.arg.base) : AFRAME.utils.coordinates.stringify(self.positionVec3('main'));
-            defaultSize = (self.arData.isMarkerType == 1 ? { w: 4, h: 2, d: 2 } : { w: 4, h: 4, d: 4 });
+            defaultSize = (self.arData.isMarkerType == 1 ? { w: 2, h: 2, d: 2 } : { w: 4, h: 4, d: 4 });
 
             self.wrap = document.createElement('a-box');
             self.wrap.setAttribute('id', 'base');
