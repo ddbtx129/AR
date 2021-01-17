0930bc9b9874694231ccdf321bfca85f05625ee7
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-17 19:36:15 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -538,7 +538,7 @@ var SizeRate = 20;
 
                 wrapPos.x -= 0;
                 wrapPos.y -= ((val.isMp4) ? 0 : 2);
-                wrapPos.z -= 8 * 10;
+                wrapPos.z -= 8 * (!!(val.isMarkerType == 1) ? 1 : 50);
 
 
                 var pvAngle = 0;
