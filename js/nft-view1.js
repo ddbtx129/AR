0886661b8b696a5af28adeee0401fc788ef7a3e8
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-18 16:32:51 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -979,8 +979,9 @@ var SizeRate = 20;
             var h1 = self.arData.size.h;
             var h1_2 = self.arData.size.h / 2;
 
-            var i = (!!(self.arg.isMarkerType == 1) ? (!!(self.arg.pv) ? 0 : 0) : (!!(self.arg.pv) ? -h1_2 : h1_2));
-                
+            //var i = (!!(self.arg.isMarkerType == 1) ? (!!(self.arg.pv) ? 0 : 0) : (!!(self.arg.pv) ? -h1_2 : h1_2));
+            var i = (!!(self.arg.isMarkerType == 1) || !!(self.arg.pv) ? 0 : h1_2);
+
             //if (type === 'shadow') {
             //    return { x: 0, y: 0, z: -h1_2 };
             //} else {
