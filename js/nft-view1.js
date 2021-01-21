1b810f19e15cf56f1af0f0ee3864274ed9f37f34
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-21 23:05:47 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -301,13 +301,30 @@ var objecttype = "png";
             var basePos = AFRAME.utils.coordinates.parse(defaultwrapPos.x + ' ' + defaultwrapPos.y + ' ' + defaultwrapPos.z);
             var baseScale = AFRAME.utils.coordinates.parse(defaultwrapScale.w + ' ' + defaultwrapScale.h + ' ' + defaultwrapScale.d);
 
+            self.wrap.basePos = basePos;
+            self.wrap.baseScale = baseScale;
+
             self.wrap = document.createElement('a-box');
+            //self.wrap.setAttribute('id', 'base');
+            //self.wrap.setAttribute('scale', AFRAME.utils.coordinates.stringify(baseScale));
+            //self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(basePos));
+            //self.wrap.setAttribute('src', rootPath + 'asset/plane.png');
+            //self.wrap.setAttribute('rotation', '0 0 0');
+            //self.wrap.setAttribute('material', 'transparent: true, opacity: 0');
+
+            this.setWeapAttribute();
+        },
+
+        setWeapAttribute: function () {
+            var self = this;
+
             self.wrap.setAttribute('id', 'base');
-            self.wrap.setAttribute('scale', AFRAME.utils.coordinates.stringify(baseScale));
-            self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(basePos));
+            self.wrap.setAttribute('scale', AFRAME.utils.coordinates.stringify(self.wrap.baseScale));
+            self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(self.wrap.basePos));
             self.wrap.setAttribute('src', rootPath + 'asset/plane.png');
             self.wrap.setAttribute('rotation', '0 0 0');
             self.wrap.setAttribute('material', 'transparent: true, opacity: 0');
+
         },
 
         createModel: function () {
