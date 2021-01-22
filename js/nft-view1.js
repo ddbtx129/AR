f8c4d37c7fa0f97a095e40c094e51f61232b534c
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-22 15:56:08 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -312,10 +312,9 @@ var objecttype = "png";
             self.wrap.setAttribute('id', 'base');
             self.wrap.setAttribute('scale', AFRAME.utils.coordinates.stringify(baseScale));
             self.wrap.setAttribute('position', AFRAME.utils.coordinates.stringify(basePos));
-            self.wrap.setAttribute('src', rootPath + 'asset/plane.png');
+            //self.wrap.setAttribute('src', rootPath + 'asset/plane.png');
             self.wrap.setAttribute('rotation', '0 0 0');
             self.wrap.setAttribute('material', 'transparent: true, opacity: 0');
-
             self.wrap.setAttribute('visible', false)
         },
 
