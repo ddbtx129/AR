9ab8929ff883b8814583b497935714588f762482
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-24 22:05:06 +0900

    Add files via upload

--- js/nft-view1.js
+++ js/nft-view1.js
@@ -933,11 +933,13 @@ var tapclicked = false;
 
                     setTimeout(function () {
                         if (tapCount == 0 && !(scalechange)) {
+                            tapclicked = false;
                             objNo = ((webArViewer.srcno.obj + 1) <= webArViewer.srcno.length) ? webArViewer.srcno.obj + 1 : 1;
                             switchObject(e, objNo);
                             return;
                         }
                         if (tapCount >= 1 && !(scalechange)) {
+                            tapclicked = false;
                             objNo = ((webArViewer.srcno.obj - 1) > 0) ? webArViewer.srcno.obj - 1 : webArViewer.srcno.length;
                             switchObject(e, objNo);
                             return;
