f3a48dce690139901760205ae23e12f5b3592904
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-01-25 10:50:24 +0900

    Add files via upload

--- js/video.event.js
+++ js/video.event.js
@@ -74,7 +74,7 @@ window.addEventListener('DOMContentLoaded', function () {
         //})
 
         ////再生可能
-        //v.addEventListener('canplay', function () {
+        //v.addEventListener('canplay', function (e) {
 
         //    text.innerHTML = '再生可能';
         //    window.alert('再生可能');
