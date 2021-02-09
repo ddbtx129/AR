08e4021ae800b71c9eddd6883c68041aa04a8cf1
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-02-09 13:54:36 +0900

    Add files via upload

--- js/ar-view.multi.js
+++ js/ar-view.multi.js
@@ -1201,6 +1201,11 @@ var viewmode = 'marker';
                 for (var i = 0; i < marker.length; i++) {
                     var j = Number(marker[i]) - 1;
                     webAr.ar.arData[j].pvAngle += angle;
+                    if((webAr.ar.arData[j].pvAngle + angle) > 360){
+                        bAr.ar.arData[j].pvAngle += (angle - 360);
+                    } else if((webAr.ar.arData[j].pvAngle + angle) < -360){
+                        bAr.ar.arData[j].pvAngle += (angle + 360);
+                    }
                     webAr.ar.arData[j].wrap.setAttribute('rotation', AFRAME.utils.coordinates.stringify((webAr.ar.arData[j].pvAngle).toString() + ' 0 0'));
                     webAr.ar.objectDataVal(webAr.ar.arData[j].zoomRateH, webAr.ar.arData[j].wrapPos, webAr.ar.arData[j].pvAngle);
                 }
