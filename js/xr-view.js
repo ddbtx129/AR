b705be264c301fda670dd4c9f8777be034f90f49
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-04-18 13:02:29 +0900

    Add files via upload

--- js/xr-view.js
+++ js/xr-view.js
@@ -2251,6 +2251,8 @@ var viewmode = 'marker';
                         var video = document.querySelector('#source' + (((j + 1) * 100) + webAr.ar.arData[j].srcno.obj).toString());
                         video.muted = !(webAr.ar.videosound == 1);
                         if (webAr.ar.videoState[j] != 2) {
+                            window.alert(j);
+                            window.alert(webAr.ar.videoState[j]);
                             video.pause();
                             document.getElementById('swPlay').style.display = 'inline';
                             document.getElementById("info1").style.display = "none";
