e2e4ab1a8c69dee0d764996028c542c437074f79
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-06 21:34:40 +0900

    Add files via upload

--- js/particle-fireworks/particle-FireWorks-init.js
+++ js/particle-fireworks/particle-FireWorks-init.js
@@ -66,9 +66,7 @@
                 console.warn('particlefirework:init:not return a object:', self.data.preset, ',use a default THREE.Points..');
             };
 
-            //self.$particlefirework = points;
             self.particlefirework = points;
-            //self.el.setObject3D('particle-firework', self.$particlefirework);
             self.el.setObject3D('particle-firework', self.particlefirework);
         },
 
@@ -91,10 +89,11 @@
             if (particlefirework.presets[self.data.preset].remove) {
                 particlefirework.presets[self.data.preset].remove.call(self, arguments);
             };
-            //if (!self.$particlefirework) {
+
             if (!self.particlefirework) {
                 return;
             };
+
             self.el.removeObject3D('particle-firework');
         },
 
@@ -119,7 +118,7 @@
         tick: defaultTick,
         update: undefined,
         author: 'fireworks',
-        desc: 'A 400X400X400 with options.color,as default preset.',
+        desc: 'A 40X40X40 with options.color,as default preset.',
     });
 
     function defaultInit() {
@@ -128,9 +127,6 @@
         var geo = new THREE.Geometry();
 
         for (var i = 0; i < count; i++) {
-            //var x = Math.random() * 400 - 200;
-            //var y = Math.random() * 400 - 200;
-            //var z = Math.random() * 400 - 200;
             var x = Math.random() * 40 - 20;
             var y = Math.random() * 40 - 20;
             var z = Math.random() * 40 - 20;
@@ -153,17 +149,16 @@
         var self = this;
         var time = arguments[0][0];
         var deltaTime = arguments[0][1];
-
-        //var verts = self.particlefirework.$geometry.vertices;
         var verts = self.particlefirework.geometry.vertices;
+
         for (var i = 0; i < verts.length; i++) {
             var vert = verts[i];
-            if (vert.y < -200) {
-                vert.y = Math.random() * 400 - 200;
+            if (vert.y < -20) {
+                vert.y = Math.random() * 40 - 20;
             }
             vert.y = vert.y - (0.1 * deltaTime);
         }
-        //self.particlefirework.$geometry.verticesNeedUpdate = true;
+
         self.particlefirework.geometry.verticesNeedUpdate = true;
     };
 
