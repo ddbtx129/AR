0e124cc9790185eacad99c2d91c2a5df5b3a0650
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-10 22:03:32 +0900

    Add files via upload

---
+++ warning.js
@@ -0,0 +1,11 @@
+﻿var warningmsg;
+
+(function () {
+
+    var msg = document.getElementById('warningmsg');
+
+    if (msg != null) {
+        msg.innerHTML = warningmsg + "<br/>" + '※ 画面をタップすると表示を開始します。';
+    }
+
+}());
