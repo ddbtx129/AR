e26773709689c2c042e5f126419572cfcd43fa11
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-10 22:30:30 +0900

    Add files via upload

--- js/share/warning.js
+++ js/share/warning.js
@@ -1,14 +1,4 @@
-﻿var warningmsg = '';
-
-(function (global) {
-
-    var msg = document.getElementById('warningmsg');
-
-    if (msg != null) {
-        msg.innerHTML = warningmsg + "<br/>" + '※ 画面をタップすると表示を開始します。';
-    }
-
-}());
+﻿var warningmsg;
 
 function message(msg) {
     warningmsg = msg;
