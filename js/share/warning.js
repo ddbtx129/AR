471a570d32474fdc5d51041be14039ed4d8d0a7f
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-10 23:08:48 +0900

    Add files via upload

--- js/share/warning.js
+++ js/share/warning.js
@@ -3,18 +3,16 @@
 
 (function () {
 
-    var self = this;
-
+    console.log(0);
     var arg = {};
-    var viewIdx = {};
-
+    console.log(0);
     var pair = location.search.substring(1).split('&');
-
+    console.log(0);
     for (var i = 0; pair[i]; i++) {
         var kv = pair[i].split('=');
         arg[kv[0]] = decodeURIComponent(kv[1]);
     }
-
+    console.log(0);
     if (!!(arg[0].msg)) {
         var maginfo = document.getElementById('warningmsg');
         console.log('1.' + arg[0].msg);
