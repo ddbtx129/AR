4e91fb7685e9cf4c23c9b96793a796bc1ca4a27c
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2021-05-11 00:07:33 +0900

    Add files via upload

--- js/share/warning.js
+++ js/share/warning.js
@@ -2,22 +2,20 @@
 
 (function () {
 
-    console.log(0);
     var arg = {};
-    console.log(0);
+
     var pair = location.search.substring(1).split('&');
-    console.log(0);
+
     for (var i = 0; pair[i]; i++) {
         var kv = pair[i].split('=');
         arg[kv[0]] = decodeURIComponent(kv[1]);
     }
-    console.log(0);
+
     if (!!(arg.msg)) {
         var maginfo = document.getElementById('warningmsg');
-        console.log('1.' + arg.msg);
         if (maginfo != null) {
             console.log('2.' + arg.msg);
-            maginfo.innerHTML = (arg.msg) + '<br/><br/>' + 'ＡＲを表示できませんでした。';
+            maginfo.innerHTML = atob(arg.msg) + '<br/><br/>' + 'ＡＲを表示できませんでした。';
         }
     }
 
